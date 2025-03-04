2824c846e2f933c75b2fa54015c3f08dc4e4c2f4
Tony Terra Nova <112224948+ItsTonyy@users.noreply.github.com>
2025-03-04 19:41:44 -0300

    grid for projects and line underneath headings

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+'use client';
 
 import { Fragment } from 'react';
 import Image from 'next/image';
@@ -43,50 +43,119 @@ export default function Home() {
           </a>
         </div>
       </header>
-      
 
       <div className='w-full border-2 border-black' />
 
       {/*First Hero*/}
-      <main id='main' className='flex justify-center items-center bg-brutalistBlue w-full h-dvh px-2'>
-        <div className='flex flew-row w-full h-[90%] mx-8'>
+      <main
+        id='main'
+        className='flex justify-center items-center bg-brutalistBlue w-full h-dvh px-2'
+      >
+        <div className='flex flew-row w-full h-full mx-8'>
           <div className='h-full w-1/2 flex items-center justify-center flex-col'>
             <div className='h-[50%] w-full flex flex-col justify-center pl-12'>
               <h2 className='text-2xl mb-2'>Hi, my name is</h2>
               <h1 className='text-6xl font-semibold mb-2'>Tony Terra Nova.</h1>
-              <h1 className='text-6xl font-semibold'>And I <ColourfulText text='build things'/> for the web.</h1>
-              <p className='mt-4 text-xl font-medium text-zinc-800'>I&apos;m a software engineer specializing in building
-               (and occasionally designing) digital experiences. Currently, I&apos;m seeking my first
+              <h1 className='text-6xl font-semibold'>
+                And I <ColourfulText text='build things' /> for the web.
+              </h1>
+              <p className='mt-4 text-xl font-medium text-zinc-800'>
+                I&apos;m a software engineer specializing in building (and occasionally
+                designing) digital experiences. Currently, I&apos;m seeking my first
                 opportunity in software engineering, specializing in web development.
               </p>
-              
             </div>
           </div>
           <div className='h-full w-1/2 flex items-center justify-center'>
-            <ImageCard imageUrl='/myImage.jpg'/>
-            <Star noise={true} index={4} size={150} className='absolute top-52 right-40'/>
+            <ImageCard imageUrl='/myImage.jpg' />
+            <Star
+              noise={true}
+              index={4}
+              size={150}
+              className='absolute top-52 right-40'
+            />
           </div>
         </div>
       </main>
 
       <div className='w-full border-[3px] border-black' />
 
-
       {/*Second Hero (About Me)*/}
-      <section id='aboutMe' className='flex justify-center items-center bg-brutalistPurple w-full h-dvh px-2'>
+      <section
+        id='aboutMe'
+        className='flex justify-center items-center bg-brutalistPurple w-full h-dvh px-2'
+      >
         <div className='w-full h-[90%] mx-8 bg-purple-500/30'>
-          <h1 className='text-5xl font-bold mb-2'>About Me</h1>
+        <h1 className='text-5xl font-bold'>About Me</h1>
+        <div className='w-full border-[1px] border-black mt-2 mb-10' />
         </div>
       </section>
 
       <div className='w-full border-[3px] border-black' />
 
       {/*Second Hero (Projects)*/}
-      <section id='projects' className='flex justify-center items-center bg-brutalistYellow w-full h-dvh px-2'>
-        <div className='w-full h-[90%] mx-8 bg-yellow-500/30'>
-          <h1 className='text-5xl font-bold mb-2'>Projects</h1>
+      <section
+        id='projects'
+        className='flex justify-center items-center bg-brutalistYellow w-full h-dvh px-2'
+      >
+        <div className='w-full h-[90%] mx-8'>
+          <h1 className='text-5xl font-bold'>Projects</h1>
+          <div className='w-full border-[1px] border-black mt-2 mb-10' />
+
+          <div className='grid grid-cols-4 gap-16'>
+            <div className='group'>
+              <div
+                className='w-[22.4rem] h-56 relative bg-white border border-black rounded-md group-hover:bg-black
+               group-hover:translate-x-[0.2rem] duration-200 ease-in-out'
+              >
+                <div
+                  className='w-full h-56 translate-x-[0.5rem] -translate-y-[0.5rem] bg-red-600 border border-black rounded-md
+                    group-hover:translate-x-[0.8rem] group-hover:-translate-y-[0.8rem] transition-transform duration-200 ease-in-out'
+                ></div>
+              </div>
+            </div>
+
+            <div className='group'>
+              <div
+                className='w-[22.4rem] h-56 relative bg-white border border-black rounded-md group-hover:bg-black
+               group-hover:translate-x-[0.2rem] duration-200 ease-in-out'
+              >
+                <div
+                  className='w-full h-56 translate-x-[0.5rem] -translate-y-[0.5rem] bg-red-600 border border-black rounded-md
+                    group-hover:translate-x-[0.8rem] group-hover:-translate-y-[0.8rem] transition-transform duration-200 ease-in-out'
+                ></div>
+              </div>
+            </div>
+            <div className='group'>
+              <div
+                className='w-[22.4rem] h-56 relative bg-white border border-black rounded-md group-hover:bg-black
+               group-hover:translate-x-[0.2rem] duration-200 ease-in-out'
+              >
+                <div
+                  className='w-full h-56 translate-x-[0.5rem] -translate-y-[0.5rem] bg-red-600 border border-black rounded-md
+                    group-hover:translate-x-[0.8rem] group-hover:-translate-y-[0.8rem] transition-transform duration-200 ease-in-out'
+                ></div>
+              </div>
+            </div>
+            <div className='group'>
+              <div
+                className='w-[22.4rem] h-56 relative bg-white border border-black rounded-md group-hover:bg-black
+               group-hover:translate-x-[0.2rem] duration-200 ease-in-out'
+              >
+                <div
+                  className='w-full h-56 translate-x-[0.5rem] -translate-y-[0.5rem] bg-red-600 border border-black rounded-md
+                    group-hover:translate-x-[0.8rem] group-hover:-translate-y-[0.8rem] transition-transform duration-200 ease-in-out'
+                ></div>
+              </div>
+            </div>    
+
+          </div>
         </div>
       </section>
+
+      <footer>
+        
+      </footer>
     </>
   );
 }
